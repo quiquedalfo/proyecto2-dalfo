ef86f2a92cdca2b6bd3e982c7a9241046c7eded2
Enrique Dalfo <dalfoenrique@gmail.com>
2022-06-29 13:15:02 -0300

    navbar

---
+++ src/components/NavBar/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className="App">
+           <ul className= "navbar-nav d-flex flex-row">
+            <li className="nav-item px-2">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item px-2">
+              <a className="nav-link" href="#">Features</a>
+            </li>
+            <li className="nav-item px-2">
+              <a className="nav-link" href="#">Pricing</a>
+            </li>
+            <li className="nav-item px-2">
+              <a className="nav-link disabled">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      );
+}
+ export default Navbar
