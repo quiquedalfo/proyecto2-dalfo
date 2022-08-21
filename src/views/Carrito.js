7628ef030bf4ac4f1448d75a61cbf40b3b431171
quiquedalfo <dalfoenrique@gmail.com>
2022-08-21 15:13:10 -0300

    ultimo push

--- src/views/Carrito.js
+++ src/views/Carrito.js
@@ -7,15 +7,14 @@ const Carrito = () => {
 
     const ids = Object.keys(context.selected); // [1,2,3]
     const products = ids.map(id => context.products.find(p => p.id == id))
-    // const products = context.products.filter(p => ids.includes(p.id))
-    console.log(products)
+    // const products = context.products.filter(p => ids.includes(p.id));
     return(
         <div>
         <div> Tu carrito </div>
         <ul>
-            <li>
+        <li>
                 
-         </li>
+        </li>
         </ul>
         <pre>{JSON.stringify(context.selected, null, 2)}</pre>
         </div>
